cfe937f6478b7e7ee105a2c5077ad978b24e7ffb
carlso6375 <78713956+carlso6375@users.noreply.github.com>
2021-02-17 12:00:00 -0300

    Add files via upload

---
+++ base de dados/cardápio/wibu.js
@@ -0,0 +1,27 @@
+const wibu = (prefix, ownerBot, botName) => {
+        return `
+「 *${botName}* 」
+
+😐 *INFO*
+  🐧 Prefix: 「  ${prefix}  」
+  🐧 Creator: ${ownerBot}
+😐 *ABOUT*
+  │
+  ├─ 🐧 ${prefix}info
+  ├─ 🐧 ${prefix}blocklist
+  ├─ 🐧 ${prefix}chatlist
+  ├─ 🐧 ${prefix}ping
+  └─ 🐧 ${prefix}bugreport
+😐 *WIBU*
+  │
+  ├─ 🐧 ${prefix}neonime
+  ├─ 🐧 ${prefix}pokemon
+  ├─ 🐧 ${prefix}loli
+  ├─ 🐧 ${prefix}waifu
+  ├─ 🐧 ${prefix}randomanime
+  ├─ 🐧 ${prefix}husbu
+  ├─ 🐧 ${prefix}husbu2
+  ├─ 🐧 ${prefix}wait
+  └─ 🐧 ${prefix}nekonime`
+}
+exports.wibu = wibu
