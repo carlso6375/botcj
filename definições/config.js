f29aae2af10b281657534c5de5f28501adc6915d
carlso6375 <78713956+carlso6375@users.noreply.github.com>
2021-02-17 12:07:05 -0300

    Add files via upload

---
+++ definições/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'CAUSS_BOT',
+        ownerName: 'causs',
+        youtube: 'https://youtube.com/channel/UCpB3qh2Sp3K23s9a2Q-Gf-g',
+        instagram: 'https://instagram.com/animexx_png?igshid=e7zziiwn3d9y',
+}
